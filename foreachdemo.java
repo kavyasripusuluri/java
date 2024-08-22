303f3426ebec49310a0d047acbae9b23b7913fc8
kavyasripusuluri <151709846+kavyasripusuluri@users.noreply.github.com>
2024-08-22 12:08:30 +0530

    Add files via upload

---
+++ foreachdemo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class foreachdemo{
+    public static void main(String args[])
+    {
+        int arr[] = {10,20,30,40,50};
+        for(int i: arr){
+            System.out.println(i+" ");
+        }
+    }
+}
