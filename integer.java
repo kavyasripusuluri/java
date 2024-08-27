65b015e5853cba92376c6d8e2f1d8dcf959e116e
kavyasripusuluri <151709846+kavyasripusuluri@users.noreply.github.com>
2024-08-27 20:48:17 +0530

    Add files via upload

---
+++ integer.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+class Integer{
+    public static void main(String args[])
+    {
+    int num;
+    System.out.println("Enter an integer: ");
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    System.out.println("Enter an integer "+num);
+    }
+}
